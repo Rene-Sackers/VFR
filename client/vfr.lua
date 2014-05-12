ac5bd8276841eb0b91dec558a61880c224cb77dc
Eraknelo <rene.sackers@live.com>
2014-05-12 18:00:36 +0200

    Initial commit

---
+++ client/vfr.lua
@@ -0,0 +1,59 @@
+class "VFR"
+
+function VFR:__init()
+	--if LocalPlayer:GetSteamId().string ~= "STEAM_0:0:16870054" then return end
+	
+	self.runways = LoadRunways()
+	self.inPlane = false
+	
+	self.planes = {
+		[59] = true,
+		[81] = true,
+		[51] = true,
+		[30] = true,
+		[34] = true,
+		[85] = true,
+		[39] = true
+	}
+	
+	self.drawColor = Color(0, 255, 0, 128)
+	
+	Events:Subscribe("LocalPlayerEnterVehicle", self, self.LocalPlayerEnterVehicle)
+	Events:Subscribe("LocalPlayerExitVehicle", function() self.inPlane = false end)
+	Events:Subscribe("Render", self, self.Render)
+end
+
+function VFR:LocalPlayerEnterVehicle(args)
+	if not IsValid(args.vehicle) then
+		self.inPlane = false
+		return
+	end
+	
+	if self.planes[args.vehicle:GetModelId()] then
+		self.inPlane = true
+	end
+end
+
+function VFR:Render()
+	if not self.inPlane or Game:GetState() ~= GUIState.Game then return end
+	
+	-- Check all runways
+	for index, data in pairs(self.runways) do
+		local center = data.center
+		local radius = data.radius
+		
+		-- Player is close enough
+		if Vector3.Distance(LocalPlayer:GetPosition(), center) <= radius then
+			local runwayLines = data.runwayLines
+			
+			-- Draw all lines
+			for index, line in pairs(runwayLines) do
+				Render:DrawLine(line.lineStart, line.lineEnd, self.drawColor)
+			end
+		end
+	end
+end
+
+Events:Subscribe("ModuleLoad", function()
+	VFR()
+end)
